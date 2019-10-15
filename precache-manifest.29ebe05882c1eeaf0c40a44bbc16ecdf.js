5840ead8849721486c32e187738f49d0ef0b9a13
Alex Hughes <alexhughes@AlexMBPro.local>
2019-10-14 22:48:05 -0700

    Updates

---
+++ precache-manifest.29ebe05882c1eeaf0c40a44bbc16ecdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c546ab7f53379d8655dd651c315a9d01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ace2ced478c456b7dca9",
+    "url": "/static/css/main.8245c52e.chunk.css"
+  },
+  {
+    "revision": "c0d59f3eaff6c323f755",
+    "url": "/static/js/2.020cf830.chunk.js"
+  },
+  {
+    "revision": "ace2ced478c456b7dca9",
+    "url": "/static/js/main.8bc871b8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
